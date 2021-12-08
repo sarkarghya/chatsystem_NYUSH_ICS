c3946ed342922f550bca4b063f528017a9392eac
Arghya Sarkar <arghya@nyu.edu>
2021-12-08 17:10:15 +0530

    project

---
+++ src_diff/client_state_machine.py
@@ -0,0 +1,207 @@
+"""
+Created on Sun Apr  5 00:00:32 2015
+
+@author: zhengzhang
+"""
+from chat_utils import *
+import json
+import encrypt
+
+class ClientSM:
+    def __init__(self, s):
+        self.state = S_OFFLINE
+        self.peer = ''
+        self.me = ''
+        self.out_msg = ''
+        self.s = s
+        self.whoOnLine = ''
+        self.private_key = (0, 0)
+        self.public_key = (0,0)
+        self.logged_in = {} #read online
+
+    def set_state(self, state):
+        self.state = state
+
+    def get_state(self):
+        return self.state
+
+    def set_myname(self, name):
+        self.me = name
+
+    def get_myname(self):
+        return self.me
+
+    def connect_to(self, peer):
+        msg = json.dumps({"action": "connect", "target": peer})
+        mysend(self.s, msg)
+        response = json.loads(myrecv(self.s))
+        if response["status"] == "success":
+            self.peer = peer
+            self.out_msg += 'You are connected with ' + self.peer + '\n'
+            #self.out_msg += 'Generating your public key and private key...\n'
+            #public_key, private_key = encrypt.generate_key()
+            #key_msg = json.dumps({"action": "send public key", "public key": public_key})
+            #self.public_key = public_key
+            #mysend(self.s, key_msg)
+            #self.private_key = private_key
+            return (True)
+        elif response["status"] == "busy":
+            self.out_msg += 'User is busy. Please try again later\n'
+        elif response["status"] == "self":
+            self.out_msg += 'Cannot talk to yourself (sick)\n'
+        else:
+            self.out_msg += 'User is not online, try again later\n'
+        return(False)
+
+    def disconnect(self):
+        msg = json.dumps({"action":"disconnect"})
+        mysend(self.s, msg)
+        self.out_msg += 'You are disconnected from ' + self.peer + '\n'
+        self.peer = ''
+
+    def proc(self, my_msg, peer_msg):
+        self.out_msg = ''
+        # public_key, private_key = encrypt.generate_key()[0], encrypt.generate_key()[1]
+#==============================================================================
+# Once logged in, do a few things: get peer listing, connect, search
+# And, of course, if you are so bored, just go
+# This is event handling instate "S_LOGGEDIN"
+#==============================================================================
+
+        if self.state == S_GAMING:
+            pass
+
+
+        if self.state == S_LOGGEDIN:
+            # todo: can't deal with multiple lines yet
+            if len(my_msg) > 0:
+
+                if my_msg == 'q':
+                    self.out_msg += 'See you next time!\n'
+                    self.state = S_OFFLINE
+
+                elif my_msg == 'time':
+                    mysend(self.s, json.dumps({"action":"time"}))
+                    time_in = json.loads(myrecv(self.s))["results"]
+                    self.out_msg += "Time is: " + time_in
+
+                elif my_msg == 'who':
+                    mysend(self.s, json.dumps({"action":"list"}))
+                    logged_in = json.loads(myrecv(self.s))["results"]
+                    self.out_msg += 'Here are all the users in the system:\n'
+                    self.out_msg += logged_in
+                    logged_in = logged_in.split(',')
+                    logged_in.remove(self.me)
+                    self.whoOnLine = logged_in
+
+
+                # elif my_msg == "who_list":
+                #     mysend(self.s, json.dumps({"action":"list"}))
+                #     logged_in = json.loads(myrecv(self.s))["results"]
+                #     self.whoOnLine = logged_in
+
+
+                elif my_msg[0] == 'c':
+                    peer = my_msg[1:]
+                    peer = peer.strip()
+                    if self.connect_to(peer) == True:
+                        self.state = S_CHATTING
+                        self.out_msg += 'Connect to ' + peer + '. Chat away!\n\n'
+                        self.out_msg += '-----------------------------------\n'
+                    else:
+                        self.out_msg += 'Connection unsuccessful\n'
+
+                elif my_msg[0] == '?':
+                    term = my_msg[1:].strip()
+                    mysend(self.s, json.dumps({"action":"search", "target":term}))
+                    search_rslt = json.loads(myrecv(self.s))["results"].strip()
+                    if (len(search_rslt)) > 0:
+                        self.out_msg += search_rslt + '\n\n'
+                    else:
+                        self.out_msg += '\'' + term + '\'' + ' not found\n\n'
+
+                elif my_msg[0] == 'p' and my_msg[1:].isdigit():
+                    poem_idx = my_msg[1:].strip()
+                    mysend(self.s, json.dumps({"action":"poem", "target":poem_idx}))
+                    poem = json.loads(myrecv(self.s))["results"]
+                    if (len(poem) > 0):
+                        self.out_msg += poem + '\n\n'
+                    else:
+                        self.out_msg += 'Sonnet ' + poem_idx + ' not found\n\n'
+
+                else:
+                    self.out_msg += menu
+
+
+            if len(peer_msg) > 0:
+                try:
+                    peer_msg = json.loads(peer_msg)
+                except Exception as err :
+                    self.out_msg += " json.loads failed " + str(err)
+                    return self.out_msg
+
+                if peer_msg["action"] == "connect":
+
+                    # ----------your code here------#
+                    #print(peer_msg)
+                    self.peer = peer_msg["from"]
+                    self.out_msg += 'Request from ' + self.peer + '\n'
+                    self.out_msg += 'Generating your public key and private key...\n'
+                    self.out_msg += 'You are connected with ' + self.peer
+                    self.out_msg += '------------------------------------\n'
+                    self.state = S_CHATTING
+
+                    # ----------end of your code----#
+
+#==============================================================================
+# Start chatting, 'bye' for quit
+# This is event handling instate "S_CHATTING"
+#==============================================================================
+        elif self.state == S_CHATTING:
+
+            if len(my_msg) > 0:     # my stuff going out
+
+                mysend(self.s, json.dumps({"action": "exchange", "from": self.me, "message": my_msg}))
+                if my_msg == 'bye':
+                    self.disconnect()
+                    self.state = S_LOGGEDIN
+                    self.peer = ''
+                elif my_msg == 'who':
+                    mysend(self.s, json.dumps({"action":"list"}))
+                    logged_in = json.loads(myrecv(self.s))["results"]
+                    self.out_msg += 'Here are all the users in the system:\n'
+                    self.out_msg += logged_in
+                    logged_in = logged_in.split(',')
+                    logged_in.remove(self.me)
+                    self.whoOnLine = logged_in
+
+            if len(peer_msg) > 0:    # peer's stuff, coming in
+                # ----------your code here------#
+                peer_msg = json.loads(peer_msg)
+                print(peer_msg["message"])
+                dec_msg = encrypt.decryption(peer_msg["message"], self.private_key)
+                if peer_msg["action"] == "exchange":
+                    ctime = time.strftime('%d.%m.%y,%H:%M', time.localtime())
+                    self.out_msg += peer_msg["from"] + '(' + ctime + ')' + '(Secured): ' + dec_msg
+                if peer_msg["action"] == "disconnect":
+                    if peer_msg['msg'] == "everyone left, you are alone":
+                        self.out_msg += peer_msg["msg"] + '\n'
+                        self.disconnect()
+                        self.state = S_LOGGEDIN
+                if peer_msg["action"] == "connect":
+                    self.out_msg += '-----------------------------------\n'
+                    self.out_msg += peer_msg["from"] + " joined \n"
+                    self.out_msg += '-----------------------------------\n'
+                # ----------end of your code----#
+                
+            # Display the menu again
+            if self.state == S_LOGGEDIN:
+                self.out_msg += menu
+#==============================================================================
+# invalid state
+#==============================================================================
+        else:
+            self.out_msg += 'How did you wind up here??\n'
+            print_state(self.state)
+
+        return self.out_msg
